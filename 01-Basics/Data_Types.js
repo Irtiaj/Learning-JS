5ca260a6e0ddf85bb9bf69588e10c22f5bce9b2c
Irtiaj <irtiaj0541@gmail.com>
2026-05-10 07:17:49 +0000

    data Types commit

--- 01-Basics/Data_Types.js
+++ 01-Basics/Data_Types.js
@@ -1,4 +1,13 @@
 "use strict"// Used to force that all the codes are treated as newer version
 //alert(3-3) , We are using nodeJS not browser
 console.log(3+3);  
-console.log("Hehe");
+console.log("Hehe"); //Code readability should e high
+
+let username = "Irtiaj"
+let age = 21
+let islogged = true
+
+// console.table([username,age,islogged])
+//null is basically a standalone datatype here. that's it for now.
+//object 
+console.log(typeof null);
