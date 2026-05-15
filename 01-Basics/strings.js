33ec2516ef60c0453f2c2bb377fe4f05c965bae8
Irtiaj <irtiaj0541@gmail.com>
2026-05-15 15:40:07 +0000

    commit

---
+++ 01-Basics/strings.js
@@ -0,0 +1,19 @@
+const name = 'Irtiaj'
+let repCount = 7
+
+console.log(`My name is ${name} & My repoc ount is ${repCount}`);
+const gameName = new String('hitesh')
+// console.log(gameName.charAt('2'));
+// console.log(gameName.indexOf('h'));
+
+//substring does not takes negative value
+//On the Contrary .slice allows taking negative index. So, basically allows taking the indexes from the end.
+
+let yahoo = "dsgygefb   kg8yfgh      "
+// console.log(yahoo.trim());
+
+let url = "yahoo.ksjdhfus.com"
+
+console.log(url.replace('ks','as'));
+console.log(url.includes('edu'));
+
